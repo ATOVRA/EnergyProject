0075c9bc74b7258208cf969a95bcf149e0fb5c47
“ATOVRA” <bashatovibragim2000@gmail.com>
2023-03-02 17:51:30 +0500

    initial commit

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./HeaderStyle.css";
 import LogoEnergy from "../Images/logo.jpg";
+import { HiMenu } from "react-icons/hi";
 
 export const Header = () => {
+  const [menuDiv, setMenuDiv] = useState(!true);
   const ScrollTop = () => {
     window.scrollTo(0, 0);
   };
   const handleIshlarimiz = () => {
     window.scrollTo(0, 1000);
-  }
+  };
   const handleHaqimizda = () => {
-    window.scrollTo(0, 2500)
-  }
+    window.scrollTo(0, 2500);
+  };
+
+  const handleMenu = () => {
+    setMenuDiv(!menuDiv);
+  };
 
   return (
     <header>
@@ -27,7 +33,11 @@ export const Header = () => {
             <li onClick={ScrollTop}>Bosh sahifa</li>
             <li onClick={handleIshlarimiz}>Ishlarimiz</li>
             <li onClick={handleHaqimizda}>Biz haqimizda</li>
-            <NavLink to={"/adminLogin"} style={{ textDecoration: "none" }}>
+            <NavLink
+              to={"/adminLogin"}
+              style={{ textDecoration: "none" }}
+              className="adminMenuLi"
+            >
               <li>Admin</li>
             </NavLink>
           </ul>
@@ -38,6 +48,17 @@ export const Header = () => {
             <div className="ruLanguage">
               <button>Ru</button>
             </div>
+          </div>
+          <HiMenu className="menuIcon" onClick={handleMenu} />
+          <div className="menuHoverDiv" style={{height: menuDiv ? '200px' : '0'}}>
+            <ul>
+              <li onClick={ScrollTop}>Bosh sahifa</li>
+              <li onClick={handleIshlarimiz}>Ishlarimiz</li>
+              <li onClick={handleHaqimizda}>Biz haqimizda</li>
+              <NavLink to={"/adminLogin"} style={{ textDecoration: "none" }}>
+                <li>Admin</li>
+              </NavLink>
+            </ul>
           </div>
         </nav>
       </div>
